5ca4e0a322afe04d4f9bc8f6c3d6c1c152b1a907
safiurahmanjami <webcoding44@gmail.com>
2025-12-30 16:24:35 +0430

    created modulses

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,19 +7,19 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-// import html from "./tech/Html.png";
-// import css from "./tech/Css.png";
-// import tailwind from "./tech/Tailwind.png";
-// import javascript from "./tech/Javascript.png";
-// import typescript from "./tech/Typescripy.png";
-// import react from "./tech/React.png";
-// import next from "./tech/Next.jpg";
-// import gsap from "./tech/Gsap.png";
-// import threejs from "./tech/Threejs.svg";
-// import redux from "./tech/Redux.png";
-// import matrail from "./tech/Matrail.png";
-// import antdesign from "./tech/Ant-design.png";
-// import git from "./tech/Git.png";
+import html from "./tech/Html.png";
+import css from "./tech/Css.png";
+import tailwind from "./tech/Tailwind.png";
+import javascript from "./tech/Javascript.png";
+import typescript from "./tech/Typescripy.png";
+import react from "./tech/React.png";
+import next from "./tech/Next.jpg";
+import gsap from "./tech/Gsap.png";
+import threejs from "./tech/Threejs.svg";
+import redux from "./tech/Redux.png";
+import matrail from "./tech/Matrail.png";
+import antdesign from "./tech/Ant-design.png";
+import git from "./tech/Git.png";
 
 
 
